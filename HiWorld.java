67cc19f1055648399a64aa5f6cefebeb550ada92
hclsprt <leonghoncheong@gmail.com>
2016-02-24 23:41:08 +0800

    update HiWorld.java

--- HiWorld.java
+++ HiWorld.java
@@ -8,12 +8,16 @@ public class HiWorld {
 
     public static void main(String[] args) {
         // Prints "Hi, World" to the terminal window.
+	String str="";
         System.out.println("Hi, World");
 	for (String s : args)
 	{
+		if (str != "") str += " ";
+		str += s;
 		System.out.println(s);
+		System.out.println(str);
 	}
-	System.out.println("Hi again");
+        System.out.println("See you soon!");
     }
 
 }

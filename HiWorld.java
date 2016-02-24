69f69762a567e4ce9b5ddae30229ce47401fb100
hclsprt <leonghoncheong@gmail.com>
2016-02-24 23:16:11 +0800

    Java file for autocompile

---
+++ HiWorld.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+ *  Compilation:  javac HiWorld.java
+ *  Execution:    java HiWorld [arg0 arg1 ...]
+ *
+ ******************************************************************************/
+
+public class HiWorld {
+
+    public static void main(String[] args) {
+        // Prints "Hi, World" to the terminal window.
+        System.out.println("Hi, World");
+	for (String s : args)
+	{
+		System.out.println(s);
+	}
+    }
+
+}
